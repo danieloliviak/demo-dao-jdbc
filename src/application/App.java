09cae559ab018405bdd8f408ed4fc9e19d808baf
danieloliviak355 <danieloliviak355@penguin>
2025-04-30 17:36:35 +0100

    sellerDaoJDBC and DaoFactory

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class App {
     public static void main(String[] args){
         Departament obj = new Departament(1, "books");
         Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.creteSellerDao();
         System.out.println(seller);
     }
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+    
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO
+    }
+
+    @Override
+    public void delete(Integer id) {
+        // TODO
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
