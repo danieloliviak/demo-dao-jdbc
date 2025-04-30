799b254365f92a782079a991dc588a62cacfd325
danieloliviak355 <danieloliviak355@penguin>
2025-04-30 12:18:19 +0100

    seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Derpartament;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args){
         Derpartament obj = new Derpartament(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
